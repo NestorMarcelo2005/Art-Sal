6269ac120ea642dac4dae8983a34b87d3ad6b4b6
Nelson Rodrigues <nelsonmacedorodrigues@gmail.com>
2025-07-24 17:38:47 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -146,4 +146,11 @@ document.addEventListener("DOMContentLoaded", function () {
       if(timelineItems.length > 0) {
         createObserver(timelineItems);
       }
+      const faqItems = document.querySelectorAll('.faq-item');
+      faqItems.forEach(item => {
+        const question = item.querySelector('.faq-question');
+        question.addEventListener('click', () => {
+          item.classList.toggle('active');
+        });
+      });
     });
